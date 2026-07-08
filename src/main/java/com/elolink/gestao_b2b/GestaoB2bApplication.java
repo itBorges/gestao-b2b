2727a1022d690c9efb9afef3c5376023b43225a4
itBorges <borgestilisaac1@gmail.com>
2026-07-08 10:27:47 -0300

    chore: inicializar projeto spring

---
+++ src/main/java/com/elolink/gestao_b2b/GestaoB2bApplication.java
@@ -0,0 +1,13 @@
+package com.elolink.gestao_b2b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoB2bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoB2bApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elolink/gestao_b2b/GestaoB2bApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elolink.gestao_b2b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoB2bApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
