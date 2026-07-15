4476b481b96c23e2ac683bbc7dd732a4a567f11b
itBorges <borgestilisaac1@gmail.com>
2026-07-15 10:20:14 -0300

    refactor(estrutura):reorganiza pacotes e classes

--- src/main/java/com/elolink/gestaob2b/entidadebase/EntidadeBase.java
+++ src/main/java/com/elolink/gestaob2b/entidadebase/EntidadeBase.java
@@ -1,13 +1,10 @@
-package com.elolink.gestao_b2b.entity;
+package com.elolink.gestaob2b.entidadebase;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
