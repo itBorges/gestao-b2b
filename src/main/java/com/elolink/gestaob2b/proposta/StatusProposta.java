04c7878efbf99fb21c95877dbc739ff6ee33c127
itBorges <borgestilisaac1@gmail.com>
2026-07-20 20:21:32 -0300

    feat(StatusProposta):cria StatusProposta

---
+++ src/main/java/com/elolink/gestaob2b/proposta/StatusProposta.java
@@ -0,0 +1,12 @@
+package com.elolink.gestaob2b.proposta;
+
+public enum StatusProposta {
+    RASCUNHO,
+    APROVACAO_PENDENTE,
+    APROVADA,
+    ENVIADA,
+    ACEITA,
+    RECUSADA,
+    CANCELADA,
+    CONVERTIDA
+}
