59a50d65f720cd44ad582510fe724443dd30d4b4
itBorges <borgestilisaac1@gmail.com>
2026-07-20 21:48:23 -0300

    feat(ItemProposta):Cria a entidade ItemProposta

---
+++ src/main/java/com/elolink/gestaob2b/itemproposta/ItemProposta.java
@@ -0,0 +1,41 @@
+package com.elolink.gestaob2b.itemproposta;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="item_proposta")
+@NoArgsConstructor
+@Getter
+public class ItemProposta {
+
+    @Column(name = "valor_unitario",nullable = false )
+    private Double valorUnitario;
+
+    @Column(name ="quantidade" ,nullable = false )
+    private BigDecimal quantidade;
+
+    @Column(name ="valor_total" ,nullable = false )
+    private Double valorTotal;
+
+    @Column(name = "observacao",nullable = false,length =500 )
+    private String observacao;
+
+    public ItemProposta(
+            Double valorUnitario,
+            BigDecimal quantidade,
+            Double valorTotal
+    ){
+        this.quantidade = quantidade;
+        this.valorTotal = valorTotal;
+        this.valorUnitario = valorUnitario;
+    }
+
+
+}
